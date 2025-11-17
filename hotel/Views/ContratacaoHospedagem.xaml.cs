c07f2b15f2e41ab00cbaac29b34145c107b57864
ryuzinho2 <ryunakajune@hotmail.com>
2025-11-17 12:55:05 -0300

    boa boa

--- hotel/Views/ContratacaoHospedagem.xaml.cs
+++ hotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using hotel.Models;
+
 namespace hotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -15,30 +17,47 @@ public partial class ContratacaoHospedagem : ContentPage
         dtpck_checkin.MinimumDate = DateTime.Now;
         dtpck_checkin.MaximumDate = DateTime.Now.AddMonths(1);
 
-        var checkinDT = dtpck_checkin.Date.GetValueOrDefault(DateTime.Now);
+        var checkinDT = dtpck_checkin.Date!.Value;
 
         dtpck_checkout.MinimumDate = checkinDT.AddDays(1);
         dtpck_checkout.MaximumDate = checkinDT.AddMonths(6);
+
     }
 
     private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
     {
-        DateTime dataCheckinDT = e.NewDate.GetValueOrDefault(DateTime.Now);
+        DateTime dataCheckinDT = e.NewDate!.Value;
 
         dtpck_checkout.MinimumDate = dataCheckinDT.AddDays(1);
         dtpck_checkout.MaximumDate = dataCheckinDT.AddMonths(6);
     }
 
 
-    private void Button_Clicked(object sender, EventArgs e)
+
+    private async void Button_Clicked(object sender, EventArgs e)
     {
         try
         {
-            Navigation.PushAsync(new HospedagemContratada());
+
+            Hospedagem h = new Hospedagem
+            {
+                QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+                QntAdultos = Convert.ToInt32(stp_adultos.Value),
+                QntCriancas = Convert.ToInt32(stp_criancas.Value),
+                DataCheckIn = dtpck_checkin.Date!.Value,
+                DataCheckOut = dtpck_checkout.Date!.Value,
+            };
+
+
+
+            await Navigation.PushAsync(new HospedagemContratada()
+            {
+                BindingContext = h
+            });
         }
         catch (Exception ex)
         {
-            DisplayAlert("Ops", ex.Message, "OK");
+            await DisplayAlert("Ops", ex.Message, "OK");
         }
     }
 }
