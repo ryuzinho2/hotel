a341d9b18ba0c9108e72ae5e01134fa829db0854
Rosana <Rosana@DESKTOP-TFTS4D7>
2025-11-16 22:12:47 -0300

    Adicionar arquivos de projeto.

---
+++ hotel/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace hotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+    }
+}
